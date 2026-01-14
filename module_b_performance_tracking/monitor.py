bfb4e67c05086ade00f3db82ca4a444f6269b62c
ChinYang123 <chaichinyang1005@gmail.com>
2026-01-14 22:54:58 +0800

    Student B: Expand monitoring to include Memory and Disk metrics

--- module_b_performance_tracking/monitor.py
+++ module_b_performance_tracking/monitor.py
@@ -3,6 +3,22 @@ import datetime
 
 def get_report():
     time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
+    disk = psutil.disk_usage('/').percent
+    report = f"--- System Status Report {time_str} ---\n"
+    report += f"CPU Usage: {cpu}%\n"
+    report += f"Memory Usage: {memory}%\n"
+    report += f"Disk Usage: {disk}%\n"
+    return report
+
+if __name__ == "__main__":
+       print(get_report())import psutil
+import datetime
+
+def get_report():
+    time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     cpu = psutil.cpu_percent(interval=1)
     return f"--- System Report {time_str} ---\nCPU Usage: {cpu}%"
 
